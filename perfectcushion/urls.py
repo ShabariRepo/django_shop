9ab2bf9e46ea4999287dadfd37f65f57c1f4839f
Shabari Shenoy <sshenoy@centrilogic.internal>
2019-07-03 10:24:53 -0400

    - shopping cart delete and reduce quantity

--- perfectcushion/urls.py
+++ perfectcushion/urls.py
@@ -19,6 +19,10 @@ from shop import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = "Quoting Tool Admin"
+admin.site.site_title = "Quoting Tool Admin Portal"
+admin.site.index_title = "Welcome to QMS Admin Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
@@ -31,4 +35,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
+
